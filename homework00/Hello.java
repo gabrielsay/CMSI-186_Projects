73a17eeba9032077e80c62983e2dd4f918ab405c
gabrielsay <gsay617@gmail.com>
2018-01-16 02:43:40 -0800

    Create Hello.java

---
+++ homework00/Hello.java
@@ -0,0 +1,10 @@
+/**
+* @author - Gabe Say
+* This will return "Hello world!"
+*/
+
+public class helloWorld {
+   public static void main( String[] args ) {
+      system.out.println("Hello world!");
+   }
+}
