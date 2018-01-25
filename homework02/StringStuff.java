426b8364e636f45ad0df39573ed3470de4251b7e
gabrielsay <gsay617@gmail.com>
2018-01-25 13:40:00 -0800

    Created StringStuff.java
    First attempt of homework02

---
+++ homework02/StringStuff.java
@@ -0,0 +1,17 @@
+/**
+* Filename: StringStuff.java
+* Name: Gabriel Say
+* Date: 01/25/2018
+* Description: This program looks at different strings, which will be input.
+*              Things that may be looked for in each program include whether
+*              a string contains vowels or if the string is a palindrome etc.
+* Notes: None
+* Warnings: None
+* Exceptions: None
+**/
+
+public class StringStuff {
+  public static void main( Strings [] args ) {
+
+    public static boolean containsVowel( String s )
+      For ( int i = 0; i < s.length ; i 
