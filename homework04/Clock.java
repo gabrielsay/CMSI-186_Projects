40e871cdb8168303beda2900461186d24e39f18b
gabrielsay <gsay617@gmail.com>
2018-03-12 20:41:26 -0700

    Added getTotalSeconds

--- homework04/Clock.java
+++ homework04/Clock.java
@@ -2,12 +2,9 @@
  *  File name     :  Clock.java
  *  Purpose       :  Provides a class defining methods for the ClockSolver class
  *  @author       :  B.J. Johnson
- *  Student Author:  Gabriel Say
  *  Date written  :  2017-02-28
  *  Description   :  This class provides a bunch of methods which may be useful for the ClockSolver class
- *                   for Homework 4, part 1.  Includes the following:
- *
- *  Notes         :  None right now.  I'll add some as they occur.
+ *  Notes         :  None
  *  Warnings      :  None
  *  Exceptions    :  IllegalArgumentException when the input arguments are "hinky"
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -16,34 +13,39 @@
  *            Rev      Date     Modified by:  Reason for change/modification
  *           -----  ----------  ------------  -----------------------------------------------------------
  *  @version 1.0.0  2017-02-28  B.J. Johnson  Initial writing and release
- *  @version 1.0.1  2018-02-27  Gabriel Say   Added int and strings to code
+ *  @version 1.0.1  2018-03-01  Gabriel Say   Added to skeletal code
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+import java.text.DecimalFormat;
 
 public class Clock {
   /**
    *  Class field definintions go here
+   *  Throws exceptions seen twice because it has to be put back into a try catch block
+   *  HH:MM:SS.SSSS Format for time
    */
    private static final double DEFAULT_TIME_SLICE_IN_SECONDS = 60.0;
    private static final double INVALID_ARGUMENT_VALUE = -1.0;
    private static final double MAXIMUM_DEGREE_VALUE = 360.0;
    private static final double HOUR_HAND_DEGREES_PER_SECOND = 0.00834;
    private static final double MINUTE_HAND_DEGREES_PER_SECOND = 0.1;
-   private string time;
-   private int hours;
-   private int minutes;
-   private int seconds;
+   private double hour;
+   private double minute;
+   private double seconds;
+   private double angle;
+   private double timeSlice;
+   private double totalSeconds;
+
 
   /**
    *  Constructor goes here
    */
-   public Clock() {
-   time    = "00:00:00.0000";
-   hours   = 00;
-   minutes = 00;
-   seconds = 00.0000;
-
+   public Clock(double angle, double timeSlice) {
+     hour    = 00;
+     minute  = 00;
+     seconds = 00;
+     totalSeconds = 00;
+     timeSlice = DEFAULT_TIME_SLICE_IN_SECONDS;
    }
-
   /**
    *  Methods go here
    *
@@ -51,7 +53,11 @@ public class Clock {
    *  @return double-precision value of the current clock tick
    */
    public double tick() {
-      return 0.0;
+     totalSeconds += timeSlice;
+     seconds  = totalSeconds;
+     minute   = (totalSeconds % 60);
+     hour     = (totalSeconds % 3600);
+      return totalSeconds;
    }
 
   /**
@@ -61,7 +67,16 @@ public class Clock {
    *  @throws  NumberFormatException
    */
    public double validateAngleArg( String argValue ) throws NumberFormatException {
-      return 0.0;
+
+     double angle = 0.0;
+
+     try { angle = Double.valueOf(argValue); }
+       catch (Exception e) { throw new NumberFormatException(); }
+
+     if (angle < 0 || angle > MAXIMUM_DEGREE_VALUE) {
+       throw new NumberFormatException();
+     }
+     return angle;
    }
 
   /**
@@ -76,7 +91,12 @@ public class Clock {
    *         to take a VERY LONG TIME to complete!
    */
    public double validateTimeSliceArg( String argValue ) {
-      return 0.0;
+     double validTimeSlice = Double.valueOf(argValue);
+     if (validTimeSlice < 0 || validTimeSlice > 1800) {
+       return -1.0;
+     } else {
+       return validTimeSlice;
+     }
    }
 
   /**
@@ -84,7 +104,9 @@ public class Clock {
    *  @return double-precision value of the hour hand location
    */
    public double getHourHandAngle() {
-      return 0.0;
+     double hourHandAngle = 0.0;
+     hourHandAngle = (totalSeconds * HOUR_HAND_DEGREES_PER_SECOND) % 360;
+      return hourHandAngle;
    }
 
   /**
@@ -92,7 +114,9 @@ public class Clock {
    *  @return double-precision value of the minute hand location
    */
    public double getMinuteHandAngle() {
-      return 0.0;
+     double minuteHandAngle = 0.0;
+     minuteHandAngle = (totalSeconds * MINUTE_HAND_DEGREES_PER_SECOND) % 360;
+      return minuteHandAngle;
    }
 
   /**
@@ -109,7 +133,7 @@ public class Clock {
    *  @return double-precision value the total seconds private variable
    */
    public double getTotalSeconds() {
-      return 0.0;
+      return totalSeconds;
    }
 
   /**
@@ -132,7 +156,7 @@ public class Clock {
       System.out.println( "\nCLOCK CLASS TESTER PROGRAM\n" +
                           "--------------------------\n" );
       System.out.println( "  Creating a new clock: " );
-      ClockEmpty clock = new ClockEmpty();
+      Clock clock = new Clock( 90, 30);
       System.out.println( "    New clock created: " + clock.toString() );
       System.out.println( "    Testing validateAngleArg()....");
       System.out.print( "      sending '  0 degrees', expecting double value   0.0" );
